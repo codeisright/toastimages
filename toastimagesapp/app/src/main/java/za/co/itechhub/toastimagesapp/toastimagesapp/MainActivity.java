8106c8d8c4cd70d27fa641fb461718f9f6291491
LEBOGO OFENTSWE RAMOSHAU <ofentswel@gmail.com>
2018-05-27 04:53:32 +0200

    Continue with android app

---
+++ toastimagesapp/app/src/main/java/za/co/itechhub/toastimagesapp/toastimagesapp/MainActivity.java
@@ -0,0 +1,17 @@
+package za.co.itechhub.toastimagesapp.toastimagesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import static za.co.itechhub.toastimagesapp.toastimagesapp.R.layout.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+
+    }
+}
